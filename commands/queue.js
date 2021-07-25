a18cdfb6bd467f0639ed65de933ad2c129e50a56
Faith <83888760+qows@users.noreply.github.com>
2021-07-25 16:08:56 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,8 +1,8 @@
+
 const distube = require('distube')
 const { MessageEmbed } = require("discord.js");
 module.exports.config = {
-    name: "queue",
-    aliases: ["q"],
+    name: "queue"
 }
 /**
  * 
@@ -12,40 +12,38 @@ module.exports.config = {
  */
 module.exports.run = async(client, message, args) =>  {
  const permissions = message.channel.permissionsFor(message.client.user);
+    if (!permissions.has(["ADD_REACTIONS", "MANAGE_MESSAGES"]))
+      return message.channel.send(`i need add_reactions permissions`);
     let queue = client.distube.getQueue(message)
-    const eb = new MessageEmbed()
-        .setTitle("Kuromi")
-        .setDescription("Theres nothing in the queue")
-        .setColor('#009BFF')
-    if (!queue) return message.channel.send(eb);
+    if (!queue) return message.channel.send(`there is nothing in the queue`);
     let currentPage = 0;
     const embeds = generateQueueEmbed(message, queue.songs);
     const queueEmbed = await message.channel.send(
-      `Current page - **${currentPage + 1}**/**${embeds.length}**`,
+      `**Current Page - ${currentPage + 1}/${embeds.length}**`,
       embeds[currentPage]
     );
     try {
-      await queueEmbed.react("⬅️");
-      await queueEmbed.react("⏹️");
-      await queueEmbed.react("➡️");
+      await queueEmbed.react(":arrow_left:");
+      await queueEmbed.react(":stop_button:");
+      await queueEmbed.react(":arrow_right:");
     } catch (error) {
       console.error(error);
       message.channel.send(error.message).catch(console.error);
     }
     const filter = (reaction, user) =>
-      ["⬅️", "⏹️", "➡️"].includes(reaction.emoji.name) && message.author.id === user.id;
+      [":arrow_left:", ":stop_button:", ":arrow_right:"].includes(reaction.emoji.name) && message.author.id === user.id;
     const collector = queueEmbed.createReactionCollector(filter, { time: 30000 });
     collector.on("collect", async (reaction, user) => {
       try {
-        if (reaction.emoji.name === "➡️") {
+        if (reaction.emoji.name === ":arrow_right:") {
           if (currentPage < embeds.length - 1) {
             currentPage++;
-            queueEmbed.edit(`Current page - **${currentPage + 1}**/**${embeds.length}**`, embeds[currentPage]);
+            queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
           }
-        } else if (reaction.emoji.name === "⬅️") {
+        } else if (reaction.emoji.name === ":arrow_left:") {
           if (currentPage !== 0) {
             --currentPage;
-            queueEmbed.edit(`Current page - **${currentPage + 1}**/**${embeds.length}**`, embeds[currentPage]);
+            queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
           }
         } else {
           collector.stop();
@@ -65,11 +63,10 @@ function generateQueueEmbed(message, queue) {
     const current = queue.slice(i, k);
     let j = i;
     k += 10;
-    const info = current.map((track) => `${++j -1} [**${track.name}**](${track.url})`).slice(1).join("\n");
+    const info = current.map((track) => `**${++j -1}.** [${track.name}](${track.url}) - \`${track.formattedDuration}\``).slice(1).join("\n");
     const embed = new MessageEmbed()
-      .setColor("#009BFF")
-      .setTitle("Kuromi")
-      .setDescription(`Current song - [**${queue[0].name}**](${queue[0].url})\n\n${info}`)
+      .setColor("#69919D")
+      .setDescription(`**Current Song - [${queue[0].name}](${queue[0].url})**\n\n${info}`)
     embeds.push(embed);
   }
   return embeds;
