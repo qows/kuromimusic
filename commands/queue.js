4f7a346a9266ae86bf8e64327d1a1d38eda6d4a9
Faith <83888760+qows@users.noreply.github.com>
2021-07-25 10:59:40 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -9,7 +9,7 @@ module.exports = {
             .setDescription("Theres nothing playing")
             .setColor('#009BFF')
         if (!queue) return message.channel.send(embed)
-        const q = queue.songs.map((song, i) => `${i === 0 ? "" : `${i}.`} **${song.name}** - \`${song.formattedDuration}\``).join("\n")
+        const q = queue.songs.map((song, i) => `${i === 0 ? "" : `${i}`} [**${song.name}**](${song.url}) | \`${song.formattedDuration}\``).join("\n")
         const embedd = new Discord.MessageEmbed()
             .setTitle("Kuromi")
             .setDescription(`${q}`)
