700f7d66ab8da788ad41d5782c238ffc2f743037
Faith <83888760+qows@users.noreply.github.com>
2021-07-25 16:41:48 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -6,36 +6,36 @@ module.exports = {
   run: async (client, message, args) => {
     const permissions = message.channel.permissionsFor(message.client.user);
     if (!permissions.has(["ADD_REACTIONS", "MANAGE_MESSAGES"]))
-      return message.channel.send(`I need add_reactions permissions`);
+      return;
     let queue = client.distube.getQueue(message)
     let currentPage = 0;
     const embeds = generateQueueEmbed(message, queue.songs);
     const queueEmbed = await message.channel.send(
-      `**Current Page - ${currentPage + 1}/${embeds.length}**`,
+      `Current Page **${currentPage + 1}**/**${embeds.length}**`,
       embeds[currentPage]
     );
     try {
-      await queueEmbed.react(":arrow_left:");
-      await queueEmbed.react(":stop_button:");
-      await queueEmbed.react(":arrow_right:");
+      await queueEmbed.react("⬅️");
+      await queueEmbed.react("⏹️");
+      await queueEmbed.react("➡️");
     } catch (error) {
       console.error(error);
       message.channel.send(error.message).catch(console.error);
     }
     const filter = (reaction, user) =>
-      [":arrow_left:", ":stop_button:", ":arrow_right:"].includes(reaction.emoji.name) && message.author.id === user.id;
+      ["⬅️", "⏹️", "➡️"].includes(reaction.emoji.name) && message.author.id === user.id;
     const collector = queueEmbed.createReactionCollector(filter, { time: 30000 });
     collector.on("collect", async (reaction, user) => {
       try {
-        if (reaction.emoji.name === ":arrow_right:") {
+        if (reaction.emoji.name === "➡️") {
           if (currentPage < embeds.length - 1) {
             currentPage++;
-            queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
+            queueEmbed.edit(`Current Page **${currentPage + 1}**/**${embeds.length}**`, embeds[currentPage]);
           }
-        } else if (reaction.emoji.name === ":arrow_left:") {
+        } else if (reaction.emoji.name === "⬅️") {
           if (currentPage !== 0) {
             --currentPage;
-            queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
+            queueEmbed.edit(`Current Page **${currentPage + 1}**/**${embeds.length}**`, embeds[currentPage]);
           }
         } else {
           collector.stop();
@@ -56,10 +56,10 @@ function generateQueueEmbed(message, queue) {
     const current = queue.slice(i, k);
     let j = i;
     k += 10;
-    const info = current.map((track) => `**${++j -1}.** [${track.name}](${track.url}) - \`${track.formattedDuration}\``).slice(1).join("\n");
+    const info = current.map((track) => `${++j -1} [**${track.name}**](${track.url}) | \`${track.formattedDuration}\``).slice(1).join("\n");
     const embed = new Discord.MessageEmbed()
       .setColor("#69919D")
-      .setDescription(`**Current Song - [${queue[0].name}](${queue[0].url})**\n\n${info}`)
+      .setDescription(`Current Song - [**${queue[0].name}**](${queue[0].url})**\n\n${info}`)
     embeds.push(embed);
   }
   return embeds;
