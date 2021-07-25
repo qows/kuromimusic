309a6aacb1c090ddf020b71a4b4cecde8bef064a
Faith <83888760+qows@users.noreply.github.com>
2021-07-24 19:14:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const DisTube = require("distube")
 const Discord = require("discord.js")
 const client = new Discord.Client();
+const fs = require("fs")
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true, leaveOnFinish: true })
 client.commands = new Discord.Collection()
 client.aliases = new Discord.Collection()
