58a9d9388be87a7491b282567b49a5aff3bd217a
Faith <83888760+qows@users.noreply.github.com>
2021-07-25 19:14:19 -0500

    Update calculator.js

--- commands/calculator.js
+++ commands/calculator.js
@@ -4,13 +4,6 @@ module.exports = {
     name: "calculator",
     aliases: ["calc"],
     run: async (client, message, args) => {
-        await Calculator({
-            message: message,
-            embed: {
-                title: 'Button Calculator',
-                color: '#009BFF',
-            },
-            invalidQuery: 'The provided equation is invalid',
-        });
-    }
+        await Calculator(message)
+          }
 };
