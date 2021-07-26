3dacc32d59aa72fa852d4a942ff81c9383a998ab
Faith <83888760+qows@users.noreply.github.com>
2021-07-25 19:04:21 -0500

    Create calculator.js

---
+++ commands/calculator.js
@@ -0,0 +1,16 @@
+const { Calculator } = require('weky');
+
+module.exports = {
+    name: "calculator",
+    aliases: ["calc"],
+    run: async (client, message, args) => {
+        await Calculator({
+            message: message,
+            embed: {
+                title: 'Button Calculator',
+                color: '#009BFF',
+            },
+            invalidQuery: 'The provided equation is invalid',
+        });
+    }
+});
